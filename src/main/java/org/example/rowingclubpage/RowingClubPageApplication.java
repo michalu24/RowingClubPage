e97e500e992e870ef233727dabc9c9ad42e14412
michalu24 <it.michal.urbanski@gmail.com>
2025-06-08 18:19:53 +0200

    Initial commit

---
+++ src/main/java/org/example/rowingclubpage/RowingClubPageApplication.java
@@ -0,0 +1,13 @@
+package org.example.rowingclubpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RowingClubPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RowingClubPageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/rowingclubpage/RowingClubPageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rowingclubpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RowingClubPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
