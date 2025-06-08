02fb8c2e5fb356b1366fea2f4b08145b4c998611
michalu24 <it.michal.urbanski@gmail.com>
2025-06-08 20:34:30 +0200

    Add main controller, Thymeleaf template, and project setup
    Introduce the `MainController` to handle root requests and return a Thymeleaf-based `index.html`. Configure Thymeleaf dependency in `pom.xml` and add properties for database connection in `application.properties`. Include a basic project structure, navigation, and assets like the logo.

---
+++ src/main/java/org/example/rowingclubpage/controllers/MainController.java
@@ -0,0 +1,13 @@
+package org.example.rowingclubpage.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
